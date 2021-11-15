3a6178bee145d57b6f3fc6b1770776110cc4bcf9
pritammimo <crpritamsaha@gmail.com>
2021-11-15 22:54:47 +0530

    push

---
+++ src/components/Toggle.js
@@ -0,0 +1,10 @@
+const Toggle=()=>{
+    return (
+        <div className="toggle-open">
+         <span></span>
+         <span></span>
+         <span></span>
+        </div>
+    )
+}
+export default Toggle
