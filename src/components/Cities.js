fdd4b2223f7abf4e31a4067cd333cd61b8e7b5ef
pritammimo <crpritamsaha@gmail.com>
2021-12-08 22:44:05 +0530

    27 complete

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -1,5 +1,9 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { BsStopwatch } from 'react-icons/bs';
 const Cities =({cities,name})=>{
+  const format=(price)=>{
+     return `${price}.00`
+  }
     return (
         <div className="cities">
            <div className="container">
@@ -14,7 +18,28 @@ const Cities =({cities,name})=>{
                          <div className="cities__body">
                            <div className="cities__body__image">
                               <LazyLoadImage src={city.image} alt={city.image}/>
+                              <div className={city.status === 'Bestselling'? 'bestselling': city.status === 'New'? 'new' : city.status=== 'Hot'?'hot':''}>{city.status}</div>
                            </div>
+                           <div className="cities__body__contents">
+                                <div className="cities__body__contents__top">
+                                  <div className="cities__body__contents__top__name">
+                                     {city.name}
+                                  </div>
+                                  <div className="cities__body__contents__top__duration">
+                                      <BsStopwatch size={18} color="#df2189"/>
+                                      <div className="cities__body__contents__top__duration__time">
+                                       {city.duration}
+                                      </div>
+                                  </div>
+                                  
+                                </div>
+                                <div className="cities__body__contents__price">
+                                <span className="cities__body__contents__price__dollar">
+                                  $
+                                </span>
+                                   {format(city.price)}
+                                </div>
+                              </div>
                          </div>
                        </div>
                     )
